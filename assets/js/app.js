8fae0275b1765d141be3ce92df00872d425080b5
OWNER <acharyaraj71@gmail.com>
2021-03-14 17:34:43 +0545

    Carousel

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    // mousewheel: true,
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
+    keyboard: {
+        enabled: true
+    }
+  });
