34ab31e9dadee40341058870f1576b3ade6c752b
OWNER <acharyaraj71@gmail.com>
2021-03-15 16:06:27 +0545

    Null

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
@@ -19,4 +19,64 @@ var swiper = new Swiper('.swiper-container', {
     keyboard: {
         enabled: true
     }
-  });
+});
+
+/**
+ * For Video Playback
+ * When User Hovers over the video, then only video 
+ * starts to play else will be paused video will be in loop
+ */
+
+const video_element = document.querySelectorAll('.swiper-slide video')
+video_element.forEach(video=>{
+  video.volume = 0
+  video.setAttribute('loop', true)
+  const parent = video.parentNode
+  // if (parent.classList.contains('swiper-slide-active')){
+  //   
+  // }
+  video.addEventListener('mouseover', ()=>{
+    if (parent.classList.contains('swiper-slide-active')){
+      
+      if (video.paused){
+        parent.querySelector('.play-button').style.display = 'none'
+        video.play()
+      }
+    }
+  })
+  
+  video.addEventListener('mouseleave', ()=>{
+    const parent = video.parentNode
+    if (parent.classList.contains('swiper-slide-active')){
+      if (!video.paused){
+        video.pause()
+        parent.querySelector('.play-button').style.display = 'flex'
+      }
+    }
+  })
+})
+
+/***
+ * Checking if Playback is stopped so that multiple video doest plays atonce
+ */
+const check_video_playback = ()=>{
+  const main_slider = document.querySelectorAll('.swiper-slide')
+  main_slider.forEach(slider_i =>{
+    const video_item = slider_i.querySelector("video")
+    if (slider_i.contains(video_item)){
+      if (!slider_i.classList.contains('swiper-slide-active')){
+        console.log(slider_i.querySelector(".play-button"));
+        slider_i.querySelector('.play-button').style.display = 'none'
+        video_item.pause()
+      }
+      else{
+        if(video_item.paused){
+          slider_i.querySelector('.play-button').style.display = 'flex'
+        }
+      }
+    }
+  })
+
+  setTimeout(check_video_playback, 1)
+}
+check_video_playback()
